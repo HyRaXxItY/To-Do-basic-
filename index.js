7ac991b2c3815aec13e1aa80e4f60475ac4e22cc
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-03-16 19:34:25 +0530

    basic todo

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const TodoTask = require('./models/todoTask')
 const methodOverride = require("method-override");
 
-const app = express();
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+
+mongoose.connect('mongodb://localhost:27017/TodoTask', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.use(methodOverride("_method"));
+
+
+
+// const addDone = () => {
+//     const element = document.querySelector(".list-item.name");
+//     element.classList.toggle("done");
+// }
+
+
+
+app.get("/", async (req, res) => {
+    const todo = await TodoTask.find({});
+    res.render('index', { todo })
+})
+
+app.post("/", async (req, res) => {
+    const todo = new TodoTask(req.body.todo);
+    await todo.save();
+    res.redirect(`/`);
+});
+
+
+app.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const todo = await TodoTask.find({});
+    const current_edit = await TodoTask.findById(id);
+    res.render('edit', { current_edit, id, todo });
+})
+
+
+app.post('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
 });
 
 app.listen(3000, () => {
