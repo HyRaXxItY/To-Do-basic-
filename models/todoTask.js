59884fb533d8f9ad718b7f7ca27b4be9c97dd760
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-03-19 21:02:13 +0530

    basic app

--- models/todoTask.js
+++ models/todoTask.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const todoTaskSchema = new mongoose.Schema({
     content: {
         type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now
+        required: true
     }
 })
 module.exports = mongoose.model('TodoTask', todoTaskSchema);
